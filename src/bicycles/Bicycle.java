6a59f01628e8fa9e9e6e329cc3676760b9ed85b1
Banele Ngwane <banelengwane@gmail.com>
2020-01-16 10:32:03 +0200

    initial commit

---
+++ src/bicycles/Bicycle.java
@@ -0,0 +1,22 @@
+package bicycles;
+
+public class Bicycle {
+    private int speed;
+
+    public void accelerate(){
+        speed += 5;
+    }
+
+    public void brake(){
+        speed -= 3;
+    }
+
+    public int currentSpeed(){
+        return speed;
+    }
+
+    public void stop(){
+        speed = 0;
+    }
+
+}
